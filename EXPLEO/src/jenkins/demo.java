27caaf3c692e2a9d62c83f69cdc762752fe4f2df
{username} <subhashreerajait@gmail.com>
2026-03-10 16:11:25 +0530

    add jenkins file

---
+++ EXPLEO/src/jenkins/demo.java
@@ -0,0 +1,17 @@
+package jenkins;
+import java.util.*;
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a name: ");
+		String name=sc.nextLine();
+		System.out.println("Enter a age: ");
+		int age=sc.nextInt();
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		
+	}
+
+}
